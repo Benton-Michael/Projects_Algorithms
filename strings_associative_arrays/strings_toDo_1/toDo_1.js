fd7a1870a1782eab70a956a9c837054790e78540
Benton-Michael <mbenton002@gmail.com>
2022-06-11 19:05:04 -0600

    Completed acronyms, countNonSpaces, removeShorterStrings

--- strings_associative_arrays/strings_toDo_1/toDo_1.js
+++ strings_associative_arrays/strings_toDo_1/toDo_1.js
@@ -53,6 +53,7 @@ function removeBlanks(str) {
 // console.log(removeBlanks("I can not BELIEVE it's not BUTTER"))
 // OUTPUTS: IcannotBELIEVEit'snotBUTTER
 
+
 // ------------------------***** Get Digits ******------------------------
 
 // Get Digits:
@@ -80,20 +81,21 @@ function getDigits(str) {
             // console.log(str[char])
             newNumStr += str[char]
     }
-    }
-        // turn our new string into a number 
-    console.log(newNumStr)
+    // turn our new string into a number 
+    // console.log(newNumStr)
     // return the new string(number) using the Number()
-    console.log(Number(newNumStr))
-
+    // console.log(Number(newNumStr))
+}
+    return Number(newNumStr)
 }
 
 // getDigits("abc8c0d1ngd0j0!8")
-getDigits("0s1a3y5w7h9a2t4?6!8?0")
+// console.log(getDigits("0s1a3y5w7h9a2t4?6!8?0"))
 // OUTPUT:
 // 01357924680 --> note: this is a string still (we can tell because of the leading 0)
 // 1357924680 --> the Number() has been run
-// The first console.log can be removed but is helpful to see the Number() working
+// The first console.log at ln81 can be removed but is helpful to see the Number() working
+// Final Output: 1357924680 when console.log on the getDigits()
 
 
 
@@ -101,14 +103,46 @@ getDigits("0s1a3y5w7h9a2t4?6!8?0")
 
 
 // Acronyms:
-// Create a function that, given a string, returns the string’s acronym (first letter of the word capitalized). You are allowed to use .split() and .toUpperCase().
+// Create a function that, given a string, returns the string’s acronym (first letter of the word capitalized). 
+// You are allowed to use .split() and .toUpperCase().
 
 // acronym(" there's no free lunch - gotta pay yer way. ") => "TNFL-GPYW". 
 
 // acronym("Live from New York, it's Saturday Night!") => "LFNYISN".
 
+// Documentation for .split() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+// Documentation for .toUpperCase() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 
 
+function acronym(str) {
+
+    // there is no need for a space in the empty string below but it doesn't affect result
+    let wordsArr = str.split(' ')
+    let acronymStr = ''
+
+    // console.log(wordsArr)
+
+
+    // loop through words array
+    for(let word in wordsArr) {
+
+        // without the check, we will see undefined on either end side of the loop
+        if(wordsArr[word].length > 0){
+        // grab the first letter of the word --> char at 0th index
+        // console.log(wordsArr[word][0])
+        acronymStr += wordsArr[word][0].toUpperCase()
+        }
+            // transform the first letter to uppercase    
+            // concatenate the letter grabbed into an acronym strings
+    }
+
+        // return acronym string
+        return acronymStr
+}
+console.log(acronym(" there's no free lunch - gotta pay yer way. "))
+// OUTPUT: TNFL-GPYW
+console.log(acronym("Live from New York, it's Saturday Night!"))
+// OUTPUT: LFNYISN
 
 
 // ------------------------***** Count non-spaces ******------------------------
@@ -120,15 +154,65 @@ getDigits("0s1a3y5w7h9a2t4?6!8?0")
 // countNonSpaces("Honey pie, you are driving me crazy") => 29
 // countNonSpaces("Hello world !") => 11
 
+function countNonSpaces(str) {
+
+    // initialize a counter variable
+    let counter = 0
 
 
+    // loop through the string
+    for(let char in str) {
+
+        // if character is not a space
+        if(str[char] != ' ') {
+
+            // increment the counter
+            counter++
+        }
+        
+    }
+    // return the counter 
+    return counter
+
+}
+console.log(countNonSpaces("Honey pie, you are driving me crazy"))
+// OUTPUT: 29
+console.log(countNonSpaces("Hello world !"))
+// OUTPUT: 11
 
 
 // ------------------------***** Remove Shorter Strings: ******------------------------
 // Remove Shorter Strings:
-// Create a function that, given an array of strings and a numerical value, returns an array that only contains strings longer than or equal to the given value.
+// Create a function that, given an array of strings and a numerical value, returns an array 
+// that only contains strings longer than or equal to the given value.
 
 // Examples:
 
 // removeShorterStrings(['Good morning', 'sunshine', 'the', 'Earth', 'says', 'hello'], 4) => ['Good morning', 'sunshine', 'Earth', 'says', 'hello']
+// in this test case "the" is not included in results --> it is 3 char
 // removeShorterStrings(['There', 'is', 'a', 'bug', 'in', 'the', 'system'], 3) => ['There', 'bug', 'the', 'system']
+
+function removeShorterStrings(arr, len) {
+
+    // initialize a new array
+    let newStrings = []
+    // since we cannot use push we need to provide the array index
+    // initialize a nextIndex variable
+    let nextIndex = 0
+
+    // loop through the given array
+    for(let value in arr) {
+        // if value is greater than or equal to the len 
+        if(arr[value].length >= len ) {
+        // add value to the new array
+        // console.log(arr[value])
+        newStrings[nextIndex++] = arr[value]
+    }
+}
+    // return the new array
+    return newStrings
+}
+console.log(removeShorterStrings(['Good morning', 'sunshine', 'the', 'Earth', 'says', 'hello'], 4))
+// OUTPUT: [ 'Good morning', 'sunshine', 'Earth', 'says', 'hello' ]
+console.log(removeShorterStrings(['There', 'is', 'a', 'bug', 'in', 'the', 'system'], 3))
+// OUTPUT: [ 'There', 'bug', 'the', 'system' ]
