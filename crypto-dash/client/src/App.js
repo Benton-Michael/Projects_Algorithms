51d7d872332d7fcde743be6ebe7f58d3738366bd
Benton-Michael <mbenton002@gmail.com>
2022-06-16 22:46:59 -0600

    addition to app.js

--- crypto-dash/client/src/App.js
+++ crypto-dash/client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import axios from "axios"; // confirm
 import DisplayAllPredictions from "./components/DisplayAllPredictions";
 import PredictionForm from "./components/PredictionForm";
 import CoinList from "./components/CoinList";
@@ -16,9 +16,11 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
+
           <Route path="/" element={<DisplayPredsAndData />} />
           <Route path="/coinData" element={<CoinList />} />
           <Route path="/new" element={<PredictionForm />} />
+
           {/* we get to edit through the displayAll page */}
           <Route path="/edit/:id" element={<EditPrediction />} />
         </Routes>
