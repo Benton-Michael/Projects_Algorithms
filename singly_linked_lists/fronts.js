9426a0451e21b2a82c09dec2f95e7563528676ca
Benton-Michael <mbenton002@gmail.com>
2022-06-18 17:12:46 -0600

    completed removeFront method

--- singly_linked_lists/fronts.js
+++ singly_linked_lists/fronts.js
@@ -1,4 +1,8 @@
-/* Question 1: Write a method that accepts a value and create a new node, 
+/* Question 1: 
+
+method addFront()
+
+Write a method that accepts a value and create a new node, 
 assign it to the list head, and return a pointer to the new head node.
 
 Examples:
@@ -39,6 +43,18 @@ class SLL {
         We creat a node containing this value, we connect node to the list and make
         the node the head of the list
     */
+
+  removeFront() {
+    if (this.head == null) {
+      // if the list is empty there is nothing to remove
+      return this.head;
+    }
+
+    var removedNode = this.head; // Have a variable hold the node we are removing
+    this.head = removedNode.next; // moves the head of the list to the second node, which will become new head
+    removedNode.next = null; // the removed node will be caught by garbage collection
+    return this.head;
+  }
 }
 // testing output
 
@@ -47,7 +63,13 @@ mySLL.addFront(10);
 mySLL.addFront(5);
 mySLL.addFront(3);
 console.log(mySLL);
-console.log(mySLL.head.next);
+mySLL.removeFront();
+
+// mySLL.addFront(10);
+// mySLL.addFront(5);
+// mySLL.addFront(3);
+console.log(mySLL);
+// console.log(mySLL.head.next);
 // OUTPUT1:
 // SLL { head: SLLNode { value: 10, next: null } }
 
@@ -62,3 +84,61 @@ console.log(mySLL.head.next);
 //   SLLNode { value: 5, next: SLLNode { value: 10, next: null } }
 
 // ----------------------------------------------------------------
+
+/* 
+Question 2: Remove Front
+method removeFront()
+
+Write a method to remove the head node and return the new list head node. 
+If the list is empty, return null.
+
+Examples:
+
+SLL1.removeFront() => Node { data: 5, next: Node { data: 18, next: null } }
+SLL1.removeFront() => Node { data: 18, next: null }
+*/
+
+// OUTPUT 1:
+/* Error related to edge case for problem 2:
+
+this.head = removedNode.next; // moves the head of the list to the second node, which will become new head
+--> the list was empty to begin with so we want to add a condition to check if the list is empty before
+we attempt the operation
+*/
+
+// OUTPUT 2: after adding the if check for an empty list
+// SLL { head: null }
+
+// OUTPUT 3: after adding front(10)
+// SLL { head: null }
+
+// OUTPUT 4: after adding the console.log
+
+// SLL { head: SLLNode { value: 5, next: null } }
+// SLL { head: null }
+
+// OUTPUT 5: after adding the second node with value 5
+
+// SLL {
+//     head: SLLNode { value: 5, next: SLLNode { value: 10, next: null } }
+//   }
+//   SLL { head: SLLNode { value: 10, next: null } }
+
+// Final OUTPUT after adding 3 nodes
+
+// SLL {
+//     head: SLLNode { value: 3, next: SLLNode { value: 5, next: [SLLNode] } }
+//   }
+//   SLL {
+//     head: SLLNode { value: 5, next: SLLNode { value: 10, next: null } }
+//   }
+
+/*
+
+Problem 2 summary: key is the confirm the list is not empty before trying to 
+remove the head
+
+*/
+
+// --------------------------------
+
